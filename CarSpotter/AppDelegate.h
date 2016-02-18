a9d4cfb2d989c1af932829409332216f40e9c61b
Jason Williams <jasonmw_nf@hotmail.com>
2016-02-18 01:39:24 -0330

    Initial Commit

---
+++ CarSpotter/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CarSpotter
+//
+//  Created by Jason Williams on 2016-02-18.
+//  Copyright © 2016 Screaming Goat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CarSpotter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarSpotter
+//
+//  Created by Jason Williams on 2016-02-18.
+//  Copyright © 2016 Screaming Goat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
